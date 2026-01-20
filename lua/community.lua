3032f4e68521d8055f100a83b62285732f7a915e
catitw <catitw@outlook.com>
2026-01-20 13:19:10 +0800

    refactor: refactor project layout

--- lua/community.lua
+++ lua/community.lua
@@ -2,9 +2,37 @@
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
 
----@type LazySpec
-return {
-  "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+local community = {
+  pack = {
+    lua = {},
+  },
 }
+
+---Convert community table to LazySpec format (supports arbitrary nesting levels)
+---@param community_table table The community configuration table
+---@return LazySpec spec The formatted LazySpec
+local function to_lazy_spec(community_table)
+  local spec = { "AstroNvim/astrocommunity" }
+
+  ---Recursively traverse the community table and build import specs
+  ---@param tbl table The current level of the community table
+  ---@param path string The accumulated path for the current level
+  local function traverse(tbl, path)
+    for key, value in pairs(tbl) do
+      local current_path = path .. "." .. key
+      if type(value) == "table" and next(value) ~= nil then
+        -- Non-empty table: nested node, continue traversing
+        traverse(value, current_path)
+      else
+        -- Empty table or non-table value: leaf node, create the import spec
+        table.insert(spec, { import = current_path })
+      end
+    end
+  end
+
+  traverse(community_table, "astrocommunity")
+  return spec
+end
+
+---@type LazySpec
+return to_lazy_spec(community)
---
+++ lua/plugins/init.lua
@@ -0,0 +1,4 @@
+return {
+  { import = "plugins.core" },
+  { import = "plugins.user" },
+}
